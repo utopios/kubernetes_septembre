19d93fecb2af7ff333e378b244cb8c1aae806adf
ihab abadi <ihab@utopios.net>
2024-09-24 13:53:16 +0200

    version 2 correction exercice 7

--- exercices/exercice7/script.sh
+++ exercices/exercice7/script.sh
@@ -1,3 +1,4 @@
-kubectl create configmap proxy-config --from-file=./nginx.conf
+## kubectl create configmap proxy-config --from-file=./nginx.conf
+
 kubectl apply -f exercice7.yaml
 kubectl port-forward services/proxy 31600:80
